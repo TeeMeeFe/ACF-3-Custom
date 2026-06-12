2f41857e167ee70c1f7497ef95b92e7042484817
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-06-12 19:23:19 -0300

    I thought i had done this one, damn you git sometimes

--- lua/acf_custom/menu/custom_engines/custom_engines.lua
+++
@@ -1,9 +0,0 @@
-local ACF = ACF
-
-if SERVER then return end -- Silence wench
-
-function CreateMenu(Menu)
-
-end
-
-ACF.AddMenuItem(299, "#acf.menu.entities", "Custom Engines", "car", CreateMenu)
--- lua/autorun/acf_custom_loader.lua
+++
@@ -1,8 +0,0 @@
-include("includes/gloader.lua")
-
--- Await till the main addon is loaded first before we go on 
-hook.Add("ACF_OnLoadAddon", "ACF Custom Loader", function()
-    gloader.Load("ACF-3-Custom", "acf_custom")
-
-    hook.Remove("ACF_OnLoadAddon", "ACF Custom Loader")
-end)
