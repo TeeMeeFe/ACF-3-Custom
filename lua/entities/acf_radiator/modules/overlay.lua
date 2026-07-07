1aed26a0631e36c10c8d1fc878709fca2b7e2175
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2026-07-07 17:08:37 -0300

    I forgot to add this before commiting, oops

--- lua/entities/acf_radiator/modules/overlay.lua
+++ lua/entities/acf_radiator/modules/overlay.lua
@@ -31,12 +31,8 @@ function ENT:ACF_UpdateOverlayState(State)
     -- Unit conversion on the temperature, from Degrees Kelvin to Celcius
     State:AddKeyValue("Temperature", ("%s°C"):format(self.Temperature - 273.15))
 
-    --if FuelType and FuelType.FuelTankOverlay then
-    --	FuelType.FuelTankOverlay(self.Amount, State)
-    --else
-        --local FuelAmount   = math.Round(self.Amount, 2)
-        --local FuelCapacity = math.Round(self.Capacity, 2)
+    local CoolantAmount   = Round(self.Amount, 2)
+    local CoolantCapacity = Round(self.Capacity, 2)
 
-        --State:AddProgressBar("Remaining Fuel", FuelAmount, FuelCapacity, " L")
-    --end
+    State:AddProgressBar("Coolant level", CoolantAmount, CoolantCapacity, " L")
 end
